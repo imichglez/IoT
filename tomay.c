328dad5fdb94829bd8d874de8f099f36376c3a4c
Ingrid González <glez.ingrid23@gmail.com>
2022-10-13 12:44:24 +0000

    practica 3

---
+++ tomay.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <ctype.h>
+#include <signal.h>
+
+int main(){
+    char c;
+    char may;
+    int n;
+
+    do{
+        n = read(STDIN_FILENO,&c,1);
+        may = toupper(c);
+        write(STDOUT_FILENO,&may,1);
+    }
+    while(n != 0);
+    return 0;
+}
